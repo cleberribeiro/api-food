600daaf6406d75ca25acfcf192928bccd852aea1
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-19 06:34:41 -0300

    chore: Adicionado configuração do Express

--- src/index.js
+++ src/index.js
@@ -9,3 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.listen(9000, () => console.log('Server started at http://localhost:9000'));
+
+app.get('/', (req, res) => {
+  return res.json({ status: "Server started at http://localhost:9000" });
+});
