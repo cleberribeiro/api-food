50614dc6837906d3f193d8a714e93550809da22e
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-20 20:28:42 -0300

    chore: remoção de linhas de códigos de teste

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
-// const app = require('./app');
 import app from './app';
 
 const PORT = 9000;
-
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
 
 app.listen(PORT, '', () => {
   return console.log(`server is listening on ${PORT}`);
