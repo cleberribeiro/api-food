9f66b049ca7e33314b79aa1df334c9c4b0611972
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-20 23:35:18 -0300

    feat: Adicionado os metodos de listagem, atualização e remoção de produtos

--- src/protocols/controller.ts
+++ src/protocols/controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 export interface Controller {
-  create(request: Request, response: Response): Response;
-  list(request: Request, response: Response): Response;
-  update(request: Request, response: Response): Response;
-  delete(request: Request, response: Response): Response;
+  create(request: Request, response: Response): Promise<Response>;
+  list(request: Request, response: Response): Promise<Response>;
+  update(request: Request, response: Response): Promise<Response>;
+  delete(request: Request, response: Response): Promise<Response>;
 }
