05a7a77cc8327dc3982e88a34171149cb2883960
Cleber <cleber@rksoftware.com.br>
2020-10-21 10:32:28 -0300

    chore: Adiconado novo arquivo de ações para os controllers

---
+++ src/protocols/actions.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+export interface OnCreate {
+  create(request: Request, response: Response): Promise<Response>;
+}
+export interface OnList {
+  list(request: Request, response: Response): Promise<Response>;
+}
+export interface OnUpdate {
+  update(request: Request, response: Response): Promise<Response>;
+}
+export interface OnDelete {
+  delete(request: Request, response: Response): Promise<Response>;
+}
