cb5efb23ab7e275e7b48e5a9057146ae9d62440c
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-21 07:07:44 -0300

    feat: Adicionado parametro opcional na rota list de produtos

--- src/controllers/produto/produto-controller.ts
+++ src/controllers/produto/produto-controller.ts
@@ -21,12 +21,23 @@ export class ProdutoController implements Controller {
       return Promise.reject(response.json(error));
     }
   }
-
+/**
+ * Este método será útil para listar os produtos ativos para venda. Ele recebe 
+ * um parametro opcional chamado status (1 ou 0), conforme o que for informado nele
+ * a query será executada levando em consideração o status de cada registro.
+ * 
+ * @param request 
+ * @param response 
+ */
   async list(request: Request, response: Response) {
     try {
-      const produtos = await connection('produtos').select('*');
-      console.log(produtos);
-      return Promise.resolve(response.json(produtos));
+      if (request.params.status && request.params.status !== undefined) {
+        const produtos = await connection('produtos').where({ status: Number(request.params.status) }).select('*');
+        return Promise.resolve(response.json(produtos));
+      } else {
+        const produtos = await connection('produtos').select('*');
+        return Promise.resolve(response.json(produtos));
+      }
     } catch (error) {
       return Promise.reject(response.json(error));
     }
@@ -42,7 +53,6 @@ export class ProdutoController implements Controller {
   async delete(request: Request, response: Response) {
     try {
       const resposta = await connection('produtos').where({ id: request.params.id }).delete();
-      console.log(resposta);
       return Promise.resolve(response.json({ message: 'Produto removido com sucesso.' }));
     } catch (error) {
       return Promise.reject(response.json(error));
--- src/routes/produto.ts
+++ src/routes/produto.ts
@@ -5,7 +5,7 @@ const produtoController = new ProdutoController();
 
 export default (router: Router): void => {
   router.post('/produtos', produtoController.create);
-  router.get('/produtos', produtoController.list);
+  router.get('/produtos/:status?', produtoController.list);
   router.put('/produtos/:id', produtoController.update);
   router.delete('/produtos/:id', produtoController.delete);
 };
