b76c9fa5eae8806ffa40178b0c6488114f1d2725
Cleber <cleber@rksoftware.com.br>
2020-10-21 08:50:20 -0300

    chore: Configurações de criação da tabela carrinhos

--- src/database/migrations/20201021083015_create_table_carrinho.ts
+++ src/database/migrations/20201021083015_create_table_carrinho.ts
@@ -1,10 +1,12 @@
-import * as Knex from "knex";
-
+import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('carrinhos', function (table) {
+    table.increments();
+    table.string('token', 40).notNullable();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('carrinhos');
 }
-
