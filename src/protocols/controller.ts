be97a46fb98e1a34a942662273802f65f69db9ab
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-20 23:09:46 -0300

    refactor: Modificação no contrato do controller

--- src/protocols/controller.ts
+++ src/protocols/controller.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 export interface Controller {
-  create(request: Request, response: Response): Promise<any>;
-  list(request: Request, response: Response): Promise<any>;
-  update(request: Request, response: Response): Promise<any>;
-  delete(request: Request, response: Response): Promise<any>;
+  create(request: Request, response: Response): Response;
+  list(request: Request, response: Response): Response;
+  update(request: Request, response: Response): Response;
+  delete(request: Request, response: Response): Response;
 }
