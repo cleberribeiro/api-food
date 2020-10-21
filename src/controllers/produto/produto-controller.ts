60a667cea071c9c011c0e0dfb16479d15ba3fa99
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-20 21:22:46 -0300

    feat: Cadastro do produto

--- src/controllers/produto/produto-controller.ts
+++ src/controllers/produto/produto-controller.ts
@@ -1,17 +1,36 @@
 import { Request, Response } from 'express';
 import { Controller } from '../../protocols/controller';
+import connection from '../../database/connection';
 export class ProdutoController implements Controller {
-    create(request: Request, response: Response) {
-        console.log(request.body);
-        return Promise.resolve(response.json({ message: 'Deu certo man...' }));
+  async create(request: Request, response: Response) {
+    try {
+
+      const { nome, descricao, preco, imagem, tags, status } = request.body;
+
+      await connection('produtos').insert({
+        nome,
+        descricao,
+        preco,
+        imagem,
+        tags,
+        status,
+      });
+
+      return Promise.resolve(response.json({ message: 'Deu certo man...' }));
+    } catch (error) {
+      return Promise.resolve(response.json(error));
     }
-    list(request: Request, response: Response) {
-        return Promise.resolve(`Chegou aqui meu velho`);
-    }
-    update(request: Request, response: Response) {
-        return Promise.resolve(null);
-    }
-    delete(request: Request, response: Response) {
-        return Promise.resolve(null);
-    }
+  }
+
+  list(request: Request, response: Response) {
+    return Promise.resolve(`Chegou aqui meu velho`);
+  }
+
+  update(request: Request, response: Response) {
+    return Promise.resolve(null);
+  }
+
+  delete(request: Request, response: Response) {
+    return Promise.resolve(null);
+  }
 }
