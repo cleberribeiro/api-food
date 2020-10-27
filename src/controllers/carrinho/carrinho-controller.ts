addad0472930be1b643c63e87901ae08455485ca
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-26 22:56:34 -0300

    feat: Cadastro de produtos no carrinho de compras

--- src/controllers/carrinho/carrinho-controller.ts
+++ src/controllers/carrinho/carrinho-controller.ts
@@ -6,38 +6,42 @@ import { v4 as uuidv4 } from 'uuid';
 export class CarrinhoController implements OnAdd {
   async add(request: Request, response: Response) {
     try {
-      // Verifica se existe um carrinho no banco de dados
-      // Caso exista o carrinho: pega o id e grava na
-      // tabela carrinho_produto junto com a quantidade e o id do produto
-      // Caso não exista deve ser cirado o carrinho, pegar o id e gravar na
-      // tabela carrinho_produto junto com a quantidade e o id do produto
+      let id;
       const { id_produto, qtd } = request.params;
-      const carrinho = await connection().from('carrinhos').select('id');
-      console.log('DBG[request.params]', request.params);
-      
-      console.log('DBG[carrinho]', carrinho);
-      // if (!carrinho.id) {
-      //   const trx = await connection.transaction();
+      const carrinho = await connection('carrinhos').select('id');
+      if (carrinho.length === 0) {
 
-      //   const books = [
-      //     {title: 'Canterbury Tales'},
-      //     {title: 'Moby Dick'},
-      //     {title: 'Hamlet'}
-      //   ];
+        const trx = await connection.transaction();
+        trx('carrinhos')
+          .insert({ token: uuidv4() }, 'id')
+          .then((id) => {
+            id = id;
+            return trx('carrinho_produto').insert({
+              id_carrinho: id,
+              id_produto: request.params.id_produto,
+              qtd: request.params.qtd,
+            });
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+        return Promise.resolve(
+          response.json({
+            id_carrinho: id,
+          })
+        );
+      } else {
+        await connection('carrinho_produto').insert({
+          id_carrinho: carrinho[0].id,
+          id_produto: request.params.id_produto,
+          qtd: request.params.qtd,
+        });
 
-      //   trx('carrinhos')
-      //     .insert({token: uuidv4() }, 'id')
-      //     .then((id) => {
-      //       const compras = { id_produto, qtd, id }
-      //       return trx('books').insert(books);
-      //     })
-      //     .then(trx.commit)
-      //     .catch(trx.rollback);
-      // }
-
-      return Promise.resolve(
-        response.json({ message: 'Produto criado com sucesso.' })
-      );
+        return Promise.resolve(
+          response.json({
+            id_carrinho: carrinho[0].id,
+          })
+        );
+      }
     } catch (error) {
       return Promise.reject(response.json(error));
     }
--- src/routes/carrinho.ts
+++ src/routes/carrinho.ts
@@ -4,5 +4,5 @@ import { CarrinhoController } from '../controllers/carrinho/carrinho-controller'
 const carrinhoController = new CarrinhoController();
 
 export default (router: Router): void => {
-  router.post('/carrinho/add-produto/:id_produto/:qtd', carrinhoController.add);
+  router.post('/carrinhos/add-produto/:id_produto/:qtd', carrinhoController.add);
 };
