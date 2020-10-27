ba4a86a149cfc72caa962afe79a4da85f8b84893
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-27 07:46:36 -0300

    chore: Adicionado controlador e rotas para o pedido (não está pronto)

--- src/controllers/pedido/pedido-controller.ts
+++ src/controllers/pedido/pedido-controller.ts
@@ -2,10 +2,45 @@ import { Request, Response } from 'express';
 import { Controller } from '../../protocols/controller';
 import connection from '../../database/connection';
 export class PedidoController {
-
   async finalizarPedido(request: Request, response: Response) {
     try {
-      
+      const MIN_PEDIDO = 10.0;
+      console.log('DBG[request]', request.body);
+      // knex.select('*').from('users').leftJoin('accounts', 'users.id', 'accounts.user_id')
+
+      const { id_carrinho, forma_pagamento, endereco_entrega } = request.body;
+
+      const carrinho = await connection
+        .from('carrinho_produto')
+        .select('*')
+        .leftJoin('produtos', 'carrinho_produto.id_produto', 'produtos.id')
+        .where({ id_carrinho: id_carrinho });
+
+      if (carrinho.length === 0) {
+        return Promise.resolve(
+          response.json({
+            message: `Não é possível finalizar o pedido, carrinho está vazio.`,
+          })
+        );
+      }
+
+      const valorPedido = carrinho
+        .map((registro) => registro.qtd * registro.preco)
+        .reduce((total, registro) => (total += registro));
+
+      if (valorPedido < MIN_PEDIDO) {
+        return Promise.resolve(
+          response.json({
+            message: `Pedido mínimo no valor de R$ ${MIN_PEDIDO}.`,
+          })
+        );
+      }
+
+      console.log('DBG.carrinho', carrinho);
+
+      // await connection('pedidos').insert({
+
+      // });
 
       return Promise.resolve(response.json({ message: 'finalizarPedidoo.' }));
     } catch (error) {
@@ -15,8 +50,6 @@ export class PedidoController {
 
   async listarPedidos(request: Request, response: Response) {
     try {
-      
-
       return Promise.resolve(response.json({ message: 'listarPedidos.' }));
     } catch (error) {
       return Promise.reject(response.json(error));
@@ -25,12 +58,11 @@ export class PedidoController {
 
   async atualizarStatusPedido(request: Request, response: Response) {
     try {
-      
-
-      return Promise.resolve(response.json({ message: 'atualizarStatusPedido.' }));
+      return Promise.resolve(
+        response.json({ message: 'atualizarStatusPedido.' })
+      );
     } catch (error) {
       return Promise.reject(response.json(error));
     }
   }
-
 }
--- src/routes/pedido.ts
+++ src/routes/pedido.ts
@@ -5,6 +5,6 @@ const pedidoController = new PedidoController();
 
 export default (router: Router): void => {
   router.post('/pedidos/finalizar-pedido', pedidoController.finalizarPedido);
-  router.get('/pedidos/finalizar-pedido', pedidoController.listarPedidos);
+  router.get('/pedidos/listar-pedido', pedidoController.listarPedidos);
   router.put('/pedidos/atualizar-status-pedido', pedidoController.atualizarStatusPedido);
 };
