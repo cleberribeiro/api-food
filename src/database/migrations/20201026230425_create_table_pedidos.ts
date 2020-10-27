2b4c344d569f2a817b941571916892c56e20eb68
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-26 23:33:23 -0300

    chore: Adicionado migration para criação da tabela de pedidos

--- src/database/migrations/20201026230425_create_table_pedidos.ts
+++ src/database/migrations/20201026230425_create_table_pedidos.ts
@@ -1,10 +1,25 @@
-import * as Knex from "knex";
-
+import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('pedidos', function (table) {
+    table.increments();
+    table.json('lista_itens').notNullable();
+    table.string('forma_pagamento', 200).notNullable();
+    table.string('endereco_entrega', 255).notNullable();
+    table.date('data_criacao').notNullable();
+    table.float('valor_total').notNullable();
+    table
+      .enu('status', [
+        'novo',
+        'aceito',
+        'saiu para entrega',
+        'entregue',
+        'cancelado',
+      ])
+      .notNullable();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('produtos');
 }
-
