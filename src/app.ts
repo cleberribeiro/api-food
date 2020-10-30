0d6d7181a51d4a3f080437479a2aa479ef83679b
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2020-10-29 23:27:13 -0300

    chore: primeiras configurações do swagger

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerConfig from './docs'
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
@@ -6,6 +8,7 @@ import routesConfig from './routes/routes.config';
 import { errors } from 'celebrate';
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
---
+++ src/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+    openapi: '3.0.0',
+    info: {
+        title: 'Food API',
+        description: 'API de controle de pedidos e cadastro de produtos.',
+        version: '1.0.0'
+    }
+}
